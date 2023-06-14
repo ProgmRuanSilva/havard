0182562d642738c286f0594afdd6499ba37d9a73
Ruan Silva <progmruansilva@gmail.com>
2023-06-14 10:22:25 -0300

    corring: end score

--- src/score.c
+++ src/score.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include "../include/mylib.h"
 
+const int N = 3;
+
+float average(int array[]);
+
 int main(void) {
-  int scores[3];
+  int scores[N];
 
   for (int i = 0; i < 3; i++) {
     scores[i] = get_int("Score: ");
   }
     
-  printf("Average: %f\n", (scores[0] + scores[1] + scores[2] / (float) 3)); 
+  printf("Average: %f\n", average(scores)); 
+}
+
+float average(int array[]) {
+  
+  int sum = 0;
+  for (int i = 0; i < N; i++) {
+    sum += array[i];
+  }
+
+  return sum / (float) N;
 }
