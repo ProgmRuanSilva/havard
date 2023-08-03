9b759995c6471caf3582e5999b6f8c473a5460c9
Ruan Silva <progmruansilva@gmail.com>
2023-08-03 12:31:22 -0300

    feature: text.py

---
+++ python/text.py
@@ -0,0 +1,18 @@
+text = "In the great green room"
+words = text.split()
+
+# Round 1
+print("Round 1")
+for word in words:
+    print(word)
+print()
+
+#Round 2
+print("Round 2")
+for word in words:
+    for c in word:
+        print(c)
+print()
+
+#Round 
+print("Round 3")
