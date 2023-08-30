758d569739d7365fdd3ea60081e067f6777ef13d
Ruan Silva <progmruansilva@gmail.com>
2023-08-30 15:25:17 -0300

    feature: froshims added

--- froshims/app.py
+++ froshims/app.py
@@ -4,17 +4,31 @@ app = Flask(__name__)
 
 REGISTRANTS = {}
 
+SPORTS = [
+        "Basketball",
+        "Soccer",
+        "Ultimate Frisbee"
+        ]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 @app.route("/register", methods=["POST"])
 def register():
+
     name = request.form.get("name")
+    if not name:
+        return render_template("failure.html")
     sport = request.form.get("sport")
+
+    if sport not in SPORTS:
+        return render_template("failure.html")
     REGISTRANTS[name] = sport
     return render_template("success.html")
 
 @app.route("/registrants")
 def registrants():
     return render_template("registrants.html", registrants=REGISTRANTS)
+
+#check cliendside and serverside together indeed
